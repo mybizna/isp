b0a64bed0aff7e4ae17c123ff5dcd528c26dc95c
Dedan Irungu <dedanirungu@gmail.com>
2022-10-27 19:35:49 +0300

    chanfws

--- Resources/views/access-payment.blade.php
+++ Resources/views/access-payment.blade.php
@@ -3,22 +3,63 @@
         <div class="w-full max-w-xs login-card">
             <h3 style="text-align: center"> - Payment - </h3>
             <div class="bg-white pt-6 mb-4">
-                <div class="mb-4">
+                <!-- Tab links -->
+                <div class="tab">
+                    <a class="tablinks active" onclick="openCity(event, 'STKPUSH')">STK Push</a>
+                    <a class="tablinks" onclick="openCity(event, 'TILLNO')">Till No</a>
+                    <a class="tablinks" onclick="openCity(event, 'PAYBILL')">Paybill</a>
+                </div>
+
+                <!-- Tab content -->
+                <div id="STKPUSH" class="tabcontent active" style="display: block">
+                    <br>
+                    <h3>MPesa STK Push</h3>
                     <small>
                         - Please Enter you <b>Phone Number</b> that you would like us to send <b>MPESA
                             STK Push.</b>
-                        <div style="text-align: center;">or</div>
+                    </small>
+                    <br>
+                    <div class="mb-4">
+                        <br>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">
+                            Phone Number
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="phone" name="phone" type="text" placeholder="Phone">
+                    </div>
+                </div>
+
+                <div id="TILLNO" class="tabcontent">
+                    <br>
+                    <h3>TILL No</h3>
+                    <small>
                         - Send Money to Till Number <b>232323</b> and enter the Mpesa Code
                     </small>
                     <br>
-                    <br>
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">
-                        Phone Number/MPESA CODE
-                    </label>
-                    <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" name="username" type="text" placeholder="Username">
+                    <div class="mb-4">
+                        <br>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="mpesa_code">
+                            MPESA CODE
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="mpesa_code" name="mpesa_code" type="text" placeholder="MPesa Code">
+                    </div>
                 </div>
+
+                <div id="PAYBILL" class="tabcontent">
+                    <br>
+                    <h3>Paybill No</h3>
+                    <small>
+                        - Send Money to Pay Bill Number <b>232323</b> and enter the Account as
+                        <b>{{ $username }}</b>
+                    </small>
+                    <br>
+                 
+                </div>
+
+
 
 
                 <div style="text-align:center; margin-top:20px;">
