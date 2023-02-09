424476dca01e5af8ad8bf08f70861c565e218f96
Dedan Irungu <dedanirungu@gmail.com>
2023-02-09 18:09:28 +0300

    change partner picker in the form

--- views/admin/subscriber/form.vue
+++ views/admin/subscriber/form.vue
@@ -6,7 +6,10 @@
                 <FormKit v-model="model.id" label="Id" id="id" type="hidden" validation="required" />
                 <FormKit v-model="model.username" label="Username" id="username" type="text" validation="required" />
                 <FormKit v-model="model.password" label="Password" id="password" type="text" validation="required" />
-                <FormKit v-model="model.partner_id" label="Partner" id="partner_id" type="text" validation="required" />
+                <FormKit button_label="Select Customer" id="partner_id" type="recordpicker"
+                    comp_url="partner/admin/partner/list.vue" :setting="setting.partner_id" v-model="model.partner_id"
+                    validation="required" inner-class="$reset formkit-inner" wrapper-class="$reset formkit-wrapper" />
+
             </div>
             <div class="col-md-6">
 
@@ -21,10 +24,17 @@ export default {
     components: {
         TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
     },
-    data () {
+    data() {
         return {
             id: null,
             path_param: ["isp", "subscriber"],
+            setting: {
+                partner_id: {
+                    path_param: ["partner", "partner"],
+                    fields: ['first_name', 'last_name', 'email'],
+                    template: '[first_name] [last_name] - [email]',
+                },
+            },
             model: {
                 id: "",
                 username: "",
