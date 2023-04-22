e5ad1c26d3f5861cd14bd4fda39c9bcc57f3b0fc
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,64 @@
+<?php
+
+$this->add_right("isp", "billing_cycle", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "billing_cycle", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "billing_cycle", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "billing_cycle", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "billing_cycle", "registered", view:true, add:true);
+$this->add_right("isp", "billing_cycle", "guest", view:true, );
+
+$this->add_right("isp", "gateway", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "gateway", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "gateway", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "gateway", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "gateway", "registered", view:true, add:true);
+$this->add_right("isp", "gateway", "guest", view:true, );
+
+$this->add_right("isp", "mac_address", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "mac_address", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "mac_address", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "mac_address", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "mac_address", "registered", view:true, add:true);
+$this->add_right("isp", "mac_address", "guest", view:true, );
+
+$this->add_right("isp", "package", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "package", "registered", view:true, add:true);
+$this->add_right("isp", "package", "guest", view:true, );
+
+$this->add_right("isp", "package_charge", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "package_charge", "registered", view:true, add:true);
+$this->add_right("isp", "package_charge", "guest", view:true, );
+
+$this->add_right("isp", "package_charge_rate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge_rate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge_rate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "package_charge_rate", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "package_charge_rate", "registered", view:true, add:true);
+$this->add_right("isp", "package_charge_rate", "guest", view:true, );
+
+$this->add_right("isp", "subscriber", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "subscriber", "registered", view:true, add:true);
+$this->add_right("isp", "subscriber", "guest", view:true, );
+
+$this->add_right("isp", "subscriber_login", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber_login", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber_login", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscriber_login", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "subscriber_login", "registered", view:true, add:true);
+$this->add_right("isp", "subscriber_login", "guest", view:true, );
+
+$this->add_right("isp", "subscription", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscription", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscription", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("isp", "subscription", "staff", view:true, add:true, edit:true);
+$this->add_right("isp", "subscription", "registered", view:true, add:true);
+$this->add_right("isp", "subscription", "guest", view:true, );
